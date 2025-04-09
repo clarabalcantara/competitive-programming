baf7441d82632b32462792124f1eae1fab54e693
oiclai <clarabpnalcantara@gmail.com>
2025-04-09 14:12:08 -0300

    :sparkles: feat: add features

---
+++ G4M3-IFPB/lucros.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> sequencia(n);
+    int valor_min = sequencia[0];
+    int lucro_max = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> sequencia[i];
+    if (n < 2) {
+        cout << 0 << endl;
+        return 0;
+    }
+    for (int i = 1; i < n; ++i) {
+        int valor_atual = sequencia[i];
+        int lucro_potencial = valor_atual - valor_min;
+        
+        if (lucro_potencial > lucro_max) {
+            lucro_max = lucro_potencial;
+        }
+        
+        if (valor_atual < valor_min) {
+            valor_min = valor_atual;
+        }
+    }
+    
+    cout << (lucro_max > 0 ? lucro_max : 0) << endl;
+    }
+    return 0;
+}
