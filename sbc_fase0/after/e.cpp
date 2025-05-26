9c6234986139b2974c68d8eef66b54784b08eba8
oiclai <clarabpnalcantara@gmail.com>
2025-05-26 18:06:37 -0300

    :truck: chore: fixing unorg files

--- sbc_fase0/after/e.cpp
+++ sbc_fase0/after/e.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 int main(){
-    int a,b; cin >> a >> b;
-    if (a==1) cout << "vivo e morto" << endl;
-    else {
-        if (b==1) cout << "vivo" << endl;
-        else cout << "morto" << endl;
-    }
+
     return 0;
 }
