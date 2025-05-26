e6c6afc955f8b0382dbd0bf8f5d698e9b5dd4049
oiclai <clarabpnalcantara@gmail.com>
2025-05-26 16:51:27 -0300

    :sparkles: feat: add sbc fase 0 done questions

--- sbc_fase0/a.cpp
+++ sbc_fase0/a.cpp
@@ -3,8 +3,21 @@ using namespace std;
 
 int main()
 {
-    int m; cin > m;
-    if (a>b) cout << "A" << endl;
-    else cout << "B" << endl;
+    int a,b; cin >> a >> b;
+    if (a==1) cout << "vivo e morto" << endl;
+    else {
+        if (b==1) cout << "vivo" << endl;
+        else cout << "morto" << endl;
+    }
     return 0;
 }
+/*
+a,b = map(int,input().split())
+if a == 1:
+    print('vivo e morto')
+else:
+    if b==1:
+        print('vivo')
+    else:
+        print('morto')
+*/
